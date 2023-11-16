4b62006a9a6a7b903cc22fd3d109e0b87793c769
Anish713 <Shresthaanish713@gmail.com>
2023-11-16 19:42:13 +0545

    learned about query parameters

--- fastapi/fastapi2/myapi.py
+++ fastapi/fastapi2/myapi.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Path, Query
+from typing import Optional
 
 app = FastAPI()
 
 students = {
-    1: {"name": "Anish", "age": 21, "year": "BCT"},
-    2: {"name": "Pragya", "age": 21, "year": "BE"},
-    3: {"name": "Safal", "age": 23, "year": "BCT"},
+    1: {"name": "Anish", "age": 21, "roll": 1},
+    2: {"name": "Pragya", "age": 21, "roll": 2},
+    3: {"name": "Safal", "age": 23, "roll": 3},
+    4: {"name": "Amit", "age": 44, "roll": 4},
 }
 
 
@@ -17,14 +19,18 @@ def index():
 # http://127.0.0.1:8000/get-student/1 or 127.0.0.1/docs for API testing
 @app.get("/get-student/{student_id}")
 def get_student(
-    student_id: int = Path(description="Id of student to fetch details", gt=0, lt=4)
+    student_id: int = Path(description="Id of student to fetch details", gt=0, lt=5)
 ):
     return students.get(student_id, {"Data": "Not Found"})
 
 
 @app.get("/get-by-name")
-def get_student(name: str = Query(description="Name of student to fetch.")):
+# def get_student(name: Optional[str] = None, test : int): #Error:  SyntaxError: non-default argument follows default argument
+# def get_student( test : int, name: Optional[str] = None): #works, not best
+def get_student(*, name: Optional[str] = None, test: int):  # best way
     for student_id in students:
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"Data": "Not Found"}
+
+
