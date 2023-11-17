d7b7c25d35c0aa676197376b3ad659a270a53619
Anish713 <Shresthaanish713@gmail.com>
2023-11-17 13:48:32 +0545

    first sample react component rendered

---
+++ Web2/my-first-app/src/Hello.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// export default function Hello() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   )
+// }
+
+// //or
+
+const Hello=()=>{
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+export default Hello;
---
+++ Web2/my-first-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Hello from './Hello'
+
+const docRoot = document.getElementById("root")
+const root = ReactDOM.createRoot(docRoot)
+
+root.render(
+    <React.StrictMode>
+        <Hello />
+
+
+    </React.StrictMode>
+)
