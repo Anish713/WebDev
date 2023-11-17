2cb94f0431b542fa59d561cbce69cd9e77807988
Anish713 <Shresthaanish713@gmail.com>
2023-11-17 16:15:35 +0545

    Need for react hooks realized

--- Web2/my-first-app/src/Hello.js
+++ Web2/my-first-app/src/Hello.js
@@ -3,17 +3,36 @@ import "./Hello.css"
 
 
 const Hello = () => {
-    const country = {
+    let country = {
         name: 'Nepal',
         description: 'Nepal is a beautiful country.'
     }
+    const buttonClick = () => {
+        console.log(country);
+        country = {
+            ...country,
+            name: 'USA'
+        }
+        console.log(country)
+    }
     return (
-        /* <></> is called as fragment, can have only 1 root element. */
         <>
-        <div className='background'>
-            <h1 className='heading'>Hello {country.name}</h1>
-            <p className='description'>{country.description}</p>
-        </div>
+            <div className='background'>
+                <h1 className='heading'>Hello {country.name}</h1>
+                <div className='description'>{country.description}</div>
+
+                <button style={
+                    {
+                        padding: "10px",
+                        background: "blue",
+                        color: "white",
+                        fontSize: "1rem"
+                    }
+                }
+onClick={buttonClick}
+                > Learn More</button>
+            </div>
+            {/* When button clicked, value changes but is not reflected in UI, so needs react hooks. */}
         </>
     )
 }
