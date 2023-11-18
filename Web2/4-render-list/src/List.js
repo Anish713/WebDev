7b5890c4b98043f74c55d37692b1e79f814aa6a6
Anish713 <Shresthaanish713@gmail.com>
2023-11-18 10:15:32 +0545

    rendering list with CSS added

--- Web2/4-render-list/src/List.js
+++ Web2/4-render-list/src/List.js
@@ -1,77 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './list.css';
 const people = [
-  {
-    id: 0, // Used in JSX as a key
-    name: 'Creola Katherine Johnson',
-    profession: 'mathematician',
-    accomplishment: 'spaceflight calculations',
-    imageId: 'https://i.imgur.com/MK3eW3As.jpg',
-  },
-  {
-    id: 1, // Used in JSX as a key
-    name: 'Mario José Molina-Pasquel Henríquez',
-    profession: 'chemist',
-    accomplishment: 'discovery of Arctic ozone hole',
-    imageId: 'https://i.imgur.com/mynHUSas.jpg',
-  },
-  {
-    id: 2, // Used in JSX as a key
-    name: 'Mohammad Abdus Salam',
-    profession: 'physicist',
-    accomplishment: 'electromagnetism theory',
-    imageId: 'https://i.imgur.com/bE7W1jis.jpg',
-  },
-  {
-    id: 3, // Used in JSX as a key
-    name: 'Percy Lavon Julian',
-    profession: 'chemist',
-    accomplishment:
-      'pioneering cortisone drugs, steroids and birth control pills',
-    imageId: 'https://i.imgur.com/IOjWm71s.jpg',
-  },
-  {
-    id: 4, // Used in JSX as a key
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-    accomplishment: 'white dwarf star mass calculations',
-    imageId: 'https://i.imgur.com/lrWQx8ls.jpg',
-  },
+    {
+        id: 0, // Used in JSX as a key
+        name: 'Creola Katherine Johnson',
+        profession: 'mathematician',
+        accomplishment: 'spaceflight calculations',
+        imageId: 'https://i.imgur.com/MK3eW3As.jpg',
+    },
+    {
+        id: 1, // Used in JSX as a key
+        name: 'Mario José Molina-Pasquel Henríquez',
+        profession: 'chemist',
+        accomplishment: 'discovery of Arctic ozone hole',
+        imageId: 'https://i.imgur.com/mynHUSas.jpg',
+    },
+    {
+        id: 2, // Used in JSX as a key
+        name: 'Mohammad Abdus Salam',
+        profession: 'physicist',
+        accomplishment: 'electromagnetism theory',
+        imageId: 'https://i.imgur.com/bE7W1jis.jpg',
+    },
+    {
+        id: 3, // Used in JSX as a key
+        name: 'Percy Lavon Julian',
+        profession: 'chemist',
+        accomplishment:
+            'pioneering cortisone drugs, steroids and birth control pills',
+        imageId: 'https://i.imgur.com/IOjWm71s.jpg',
+    },
+    {
+        id: 4, // Used in JSX as a key
+        name: 'Subrahmanyan Chandrasekhar',
+        profession: 'astrophysicist',
+        accomplishment: 'white dwarf star mass calculations',
+        imageId: 'https://i.imgur.com/lrWQx8ls.jpg',
+    },
 ];
 
+export default function List() {
 
-export default function List(){
-    const [list, setList] = useState(people);
-    const [filter, setFilter] = useState('');
-    
-    useEffect(() => {
-        const filteredList = people.filter((person) => {
-        return person.name.toLowerCase().includes(filter.toLowerCase());
-        });
-        setList(filteredList);
-    }, [filter]);
-    
-    return (
-        <div>
-        <input
-            type="text"
-            placeholder="Search"
-            value={filter}
-            onChange={(event) => setFilter(event.target.value)}
-        />
-        <ul>
-            {list.map((person) => (
-            <li key={person.id}>
-                <img src={person.imageId} alt={person.name} />
-                <div>
-                <h3>{person.name}</h3>
+    const listItems = people.map((item, index) => {
+        return (
+            <li key={item.id}>
+                <img src={item.imageId} alt={item.name} />
                 <p>
-                    {person.profession} - {person.accomplishment}
+                    <h2>{item.name}</h2>
+                    <p>{item.profession}</p>
+                    <p>{item.accomplishment}</p>
+                    {/* <button onClick={() => deleteItem(item.id)}>Delete</button> */}
                 </p>
-                </div>
             </li>
-            ))}
-        </ul>
+        );
+    });
+
+    return (
+        <div className='center-view'>
+            <h1>Render List</h1>
+            <ul>{listItems}</ul>
         </div>
     );
 }
