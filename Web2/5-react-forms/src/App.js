aaad5fe5a9851b1c9498c9f4cca4fd3193f1c408
Anish713 <Shresthaanish713@gmail.com>
2023-11-18 11:45:19 +0545

    Simple form validation added using 3rd party Yup,Formik

--- Web2/5-react-forms/src/App.js
+++ Web2/5-react-forms/src/App.js
@@ -1,27 +1,65 @@
-import logo from './logo.svg';
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css';
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
 
 function App() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
 
-  const submitForm =(e)=>{
+
+  const userSchema = Yup.object({
+    name: Yup.string().required().min(2, 'Name must be more than 2 characters.'),
+    email: Yup.string().email().required(),
+    password: Yup.string().required().min(5, 'Password must be more than 5 characters.').max(15, 'Password should be smaller than 15 characters.'),
+  })
+
+
+  const submitForm = (e) => {
     e.preventDefault();
-    console.log({email,password});
-
+    console.log({ email, password });
   }
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+    validationSchema: userSchema,
+    onSubmit: (values) => {
+      console.log(values);
+      //validated values, can be used for API calls
+    },
+  })
+
+  const { errors, touched, getFieldProps } = formik;
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Forms in React</h1>
-        <form onSubmit={submitForm}>
+
+        <form noValidate onSubmit={formik.handleSubmit}>
+          {/* //calls onSubmit */}
           <div>
-            <input type='email' onChange={e=>setEmail(e.target.value)} name='email' id='email' placeholder='Email'/>
+            <input type='string' name='name' id='name' placeholder='Name' {...getFieldProps("name")} />
           </div>
+
+          <div className='error'>{errors.name}</div>
+
           <div>
-            <input type='password' onChange={e=>setPassword(e.target.value)}name='password' id='password' placeholder='Password'/>
+            <input type='email' name='email' id='email' placeholder='Email' {...getFieldProps("email")} />
           </div>
+          <div className='error'>{errors.email}</div>
+
+          <div>
+            <input type='password' name='password' id='password' placeholder='Password' {...getFieldProps("password")} />
+          </div>
+          <div className='error'>{errors.password}</div>
+
           <button type='submit'>Login</button>
         </form>
       </header>
