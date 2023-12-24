6ad0f3a1bbf0888368a875e56c380561c5b81f35
Anish713 <Shresthaanish713@gmail.com>
2023-12-24 12:56:50 +0545

    todo with local storage

--- Web3/React/10-todo-localStorage/src/App.jsx
+++ Web3/React/10-todo-localStorage/src/App.jsx
@@ -31,7 +31,17 @@ function App() {
         } : prevTodo))
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
 
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
 
 
